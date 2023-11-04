9ca727dbce57b34a92bf4bd43a5103ee0faf5a02
willamessantos <willamessantos@live.com>
2023-11-04 11:13:03 -0300

    crate MongoCreateUserRepository

---
+++ src/controller/createUsers/protocols.ts
@@ -0,0 +1,14 @@
+import { User } from "../../models/user";
+
+export interface CreateUserParams {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export interface ICreateUsersRepository {
+  createUser(params: CreateUserParams): Promise<User>;
+}
+
+export interface ICreateUsersController {}
