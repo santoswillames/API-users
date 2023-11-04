ee5cdd257fc667d307ba04e8a2d2875a2cd0ca63
willamessantos <willamessantos@live.com>
2023-11-04 10:52:23 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/getUsers/mongoGetUsers.ts
+++ src/repositories/getUsers/mongoGetUsers.ts
@@ -1,15 +1,17 @@
 import { IGetUsersRepository } from "../../controller/getUsers/protocols";
+import { MongoClient } from "../../databse/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "will",
-        lastName: "santos",
-        email: "will@teste.com",
-        password: "123",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
