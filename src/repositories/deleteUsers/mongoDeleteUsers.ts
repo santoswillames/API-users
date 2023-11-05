dfa096d47f2fd80fc34cf8027a6aa83c15c8184f
willamessantos <willamessantos@live.com>
2023-11-05 15:11:30 -0300

    create MongoDeleteUserRepository

---
+++ src/repositories/deleteUsers/mongoDeleteUsers.ts
@@ -0,0 +1,28 @@
+import { ObjectId } from "mongodb";
+import { IDeleteUserRepository } from "../../controller/deleteUsers/protocols";
+import { MongoClient } from "../../databse/mongo";
+import { User } from "../../models/user";
+
+export class MongoDeleteUserRepository implements IDeleteUserRepository {
+  async deleteUser(id: string): Promise<User> {
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("user")
+      .findOne({ _id: new ObjectId(id) });
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+
+    const { deletedCount } = await MongoClient.db
+      .collection("user")
+      .deleteOne({ _id: new ObjectId(id) });
+
+    if (deletedCount === 0) {
+      throw new Error("User not deleted.");
+    }
+
+    const { _id, ...rest } = user;
+
+    return { id: _id.toHexString(), ...rest };
+  }
+}
